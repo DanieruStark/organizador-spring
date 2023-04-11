c3c559b3e730bf700f1573393b28f92b87e63436
Daniel Santana <danielmaster19@gmail.com>
2023-04-11 07:00:12 -0300

    criado metodo put para editar

--- src/main/java/com/daniel/organizadorspring/controller/DespesaController.java
+++ src/main/java/com/daniel/organizadorspring/controller/DespesaController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -31,15 +32,29 @@ public class DespesaController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Despesa> findById(@PathVariable Long id){
+	public ResponseEntity<Despesa> findById(@PathVariable Long id) {
 		return despesaRepository.findById(id)
-		.map(rec -> ResponseEntity.ok().body(rec))
-		.orElse(ResponseEntity.notFound().build());
+				.map(rec -> ResponseEntity.ok().body(rec))
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Despesa create(@RequestBody Despesa despesa){
+	public Despesa create(@RequestBody Despesa despesa) {
 		return despesaRepository.save(despesa);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Despesa> update(@PathVariable Long id, @RequestBody Despesa despesa) {
+		return despesaRepository.findById(id)
+				.map(rec -> {
+					rec.setName(despesa.getName());
+					rec.setCategory(despesa.getCategory());
+					rec.setPrice(despesa.getPrice());
+
+					Despesa updated = despesaRepository.save(rec);
+					return ResponseEntity.ok().body(updated);
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
